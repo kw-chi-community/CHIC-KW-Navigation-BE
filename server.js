ee092453800ed5bf629b3e233716a2362f3681a9
Dowon-Study <dowon_9898@naver.com>
2025-02-06 17:24:12 +0900

    app.js 삭제

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-
-// app.get('/', (req, res) => {
-//   res.send('Hello, World!');
-// });
-
-// app.listen(PORT, () => {
-//   console.log(`🚀 App listening on port ${PORT} 🚀`);
-// });
-
 const express = require('express');
 const dotenv = require('dotenv');
 const cors = require('cors');
